e5ed5ceb8453d55c336a0690ac966d0195999211
吉村侯太朗 <youwatanabeoku@yahoo.co.jp>
2021-03-22 07:58:51 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,12 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def update
+    @book = Book.find(params[:id])
   end
 
   def destroy
