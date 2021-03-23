109f24cf63259b7ca67a5134b1f53fd5ede6e03f
吉村侯太朗 <youwatanabeoku@yahoo.co.jp>
2021-03-23 08:01:41 +0000

    Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     if @user == current_user
       render "edit"
     else
-      redirect_to edit_user_path(@user.id)
+      redirect_to user_path(current_user)
     end
   end
 
