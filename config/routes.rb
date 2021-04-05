11ca2c57f301b9fdad55bd37087ffea3aade2de6
吉村侯太朗 <youwatanabeoku@yahoo.co.jp>
2021-04-05 10:13:49 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'homes/index'
   devise_for :users
   resources :books do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create,:destroy]
   end
   resources :users
