671b8b720daa33d756515884071d0c8aaedd52ab
吉村侯太朗 <youwatanabeoku@yahoo.co.jp>
2021-04-07 05:53:31 +0000

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :follower,class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
+  has_many :followed,class_name:"Relationship",foreign_key:"followed_id",dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :books, dependent: :destroy
   attachment :profile_image
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following_user.include?(user)
+  end
 
   validates :name,presence:true,uniqueness: true,length:{in:2..20}
   validates :introduction,length:{maximum:50}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to:'homes#index'
+  post 'follow/:id' => 'relationships#follow',as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow',as: 'unfollow'
   get 'home/about' => 'homes#about'
   get 'homes/index'
   devise_for :users
